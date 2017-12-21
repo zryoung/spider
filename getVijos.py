d285e19a71552b3d14340e33eba2ab30bdc9cb4b
zryoung <riyang@qq.com>
2017-12-21 22:52:03 +0800

    Update getVijos.py

--- getVijos.py
+++ getVijos.py
@@ -6,8 +6,9 @@
 # @Version : $Id$
 
 import os
+import codecs
 
-tagdic={'title':'title.txt',\
+tagdic={#'title':'title.txt',\
 #'time_limit':'time_limit unit="s"',\
 #'memory_limit':'memory_limit unit="mb"',\
 'description':'Description.txt',\
@@ -21,18 +22,31 @@ tagdic={'title':'title.txt',\
 'hint':'Hint.txt',\
 'source':'Source.txt'
 }
-xml='''
-<?xml version="1.0" encoding="UTF-8"?>
+xml='''<?xml version="1.0" encoding="UTF-8"?>
 <fps version="1.2" url="https://github.com/zhblue/freeproblemset/">
 	<generator name="HUSTOJ" url="https://github.com/zhblue/hustoj/"/>
 '''
-rootpath=r'e:/work/myoi/vijos/新建文件夹/'
-# print(os.listdir(rootpath))
+rootpath='E:/OI/vijos_spider/'
+pNum=0
+
+xmlfile=''
+endflag=False
+#print(os.listdir(rootpath))
 for dirname in os.listdir(rootpath):
 	probPath=rootpath+dirname
-	# print(probPath)
+	#print(probPath)
 	if os.path.isdir(probPath):
-		probText='<item>'
+		
+		if pNum%20==0:
+			xmlfile=codecs.open(rootpath+'/fps_'+dirname+'_20.xml','w','utf-8')
+			xmlfile.write(xml)
+			endflag=False
+		# xml+='<item>'
+		# xml+='<title><![CDATA['+dirname+']]></title>'
+		xmlfile.write('<item>')
+		xmlfile.write('<title><![CDATA[vijos_'+dirname+']]></title>')
+		xmlfile.write('<time_limit unit="s"><![CDATA[1]]></time_limit>')
+		xmlfile.write('<memory_limit unit="mb"><![CDATA[256]]></memory_limit>')
 		fname=os.listdir(probPath)
 		for tag in tagdic:
 			# print(tag,tagdic[tag])
@@ -40,8 +54,9 @@ for dirname in os.listdir(rootpath):
 			#print(fxname)
 			if os.path.exists(fxname):
 				ftext=open(fxname,'r').read()
-				data='<'+tag+'><![CDATA['+ftext+']]></'+tag+'>'
-				print(data)
+				# xml+='<'+tag+'><![CDATA['+ftext+']]></'+tag+'>'
+				xmlfile.write('<'+tag+'><![CDATA['+ftext+']]></'+tag+'>')
+				#print(data)
 		infiles=[]
 		oufiles=[]
 		if os.path.exists(probPath+'/Config.ini'):
@@ -53,7 +68,29 @@ for dirname in os.listdir(rootpath):
 				oufiles.append(s[1])
 		# print(infiles)
 		# print(oufiles)
+		
 		indatas=[]
 		oudatas=[]
-
-		break
+		for f in infiles:
+			dtxt=open(probPath+'/Input/'+f,'r').read()
+			indatas.append(dtxt)
+		for f in oufiles:
+			dtxt=open(probPath+'/Output/'+f,'r').read()
+			oudatas.append(dtxt)
+		for i in range(len(indatas)):
+			xmlfile.write('<test_input><![CDATA['+indatas[i]+']]></test_input>')
+			xmlfile.write('<test_output><![CDATA['+oudatas[i]+']]></test_output>')
+			# xml+='<test_input><![CDATA['+indatas[i]+']]></test_input>'
+			# xml+='<test_output><![CDATA['+oudatas[i]+']]></test_output>'
+		# xml+='</item>'
+		xmlfile.write('</item>')
+		if pNum%20==19:
+			xmlfile.write('</fps>')
+			xmlfile.close()
+			endflag=True
+		pNum+=1
+# xml+='</fps>'
+if not endflag:
+	xmlfile.write('</fps>')
+	xmlfile.close()
+	endflag=True
