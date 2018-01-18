fab70609c12696311663d0c0a4a20c65ec75a4a0
riyang <riyang@qq.com>
2018-01-18 17:37:28 +0800

    change to confirm PEP8 add function get_test_info(url) get_data(url)

--- get_loj2.py
+++ get_loj2.py
@@ -5,64 +5,97 @@
 # @Link    : ${link}
 # @Version : $Id$
 
-import requests
+'''
+get problem from loj		https://loj.ac
+problem list url:			https://loj.ac/problems
+problem page url:			https://loj.ac/problem/{pid}
+problem test data url:		https://loj.ac/problem/{pid}/testdata
+problem additional_file url:https://loj.ac/problem/{pid}/additional_file
+'''
+
 import re
-import sys
-from bs4 import BeautifulSoup
-from PIL import Image
-#from io import StringIO
-import io
 from urllib import request
 import base64
+from bs4 import BeautifulSoup
+#from io import StringIO
+import requests
+
 
 class Problem(object):
-	"""docstring for Problem"""
-	def __init__(self):
-		self.title=''
-		self.description=''
-		self.inputFormat=''
-		self.outputFormat=''
-		self.sample=[]
-		self.dataRange=''
-		self.hint=''
-		self.tag=[]
-		self.source=''
-		self.workPath=''
-		self.session=requests.Session()
+    """docstring for Problem"""
 
-	def writeFps(self,ProblemCount=10):
-		pass
+    def __init__(self):
+        self.title = ''
+        self.description = ''
+        self.inputFormat = ''
+        self.outputFormat = ''
+        self.sample = []
+        self.dataRange = ''
+        self.hint = ''
+        self.tag = []
+        self.source = ''
+        self.workPath = ''
+        self.session = requests.Session()
 
-	def getImageToBase64(self,url):
-		base64_data='data:image/png;base64,'+base64.b64encode(request.urlopen(url).read()).decode('utf-8')
-		return(base64_data)
 
-	def getProblemID(self):
-		page=1
-		problemlist=[]
-		reg = '<a style="vertical-align: middle; " href="/problem/([0-9]+)">'
-		com = re.compile(reg)
-		while page<=7:
-			response=session.get("https://loj.ac/problems?page="+str(page))
-			if response.status_code==200:
-				problemlist+=re.findall(com,page)
-				page+=1
-			else:
-				print("ERROR")
-		return(problemlist)
+def write_fps(ProblemCount=10):
+    pass
 
-	def getpage(self,url):
-		try:
-			#url="https://loj.ac/problem/"+str(problemid)
-			page=session.get(url)
-		except Exception as e:
-			print(e)
 
-		# def getImageToBase64(self,url):
-		# 	file=cStringIO.StringIO(urllib2.urlopen(url).read())
-		# 	img=Image.open(file)
-		# 	base64_data=base64.b64encode(img)
-		# 	return(base64_data)
+def get_image_to_base64(url):
+    '''get image and change to base64,from url'''
+    base64_data = 'data:image/png;base64,' + \
+        base64.b64encode(request.urlopen(url).read()).decode('utf-8')
+    return base64_data
 
-p=Problem()
-print(p.getImageToBase64("https://ooo.0o0.ooo/2017/06/10/593bcc13da98c.png"))
+
+def get_problem_id_list():
+    '''get problem IDs list from page's url
+       TODO:use argument 'url' '''
+    page = 1
+    problemlist = []
+    reg = '<a style="vertical-align: middle; " href="/problem/([0-9]+)">'
+    com = re.compile(reg)
+    while page <= 7:
+        response = session.get("https://loj.ac/problems?page=" + str(page))
+        if response.status_code == 200:
+            problemlist += re.findall(com, page)
+            page += 1
+        else:
+            print("ERROR")
+    return problemlist
+
+
+def get_page(url):
+    try:
+        # url="https://loj.ac/problem/"+str(problemid)
+        page = session.get(url)
+    except Exception as e:
+        print(e)
+
+
+def get_data(url):
+    '''get test data from url'''
+    data = request.urlopen(url).read().decode('utf-8')
+    return data
+
+
+def get_test_info(url):
+    '''get test cases' info from url
+    return list like [[a1.in,a1.out]]'''
+    page = request.urlopen(url).read()
+
+    soup = BeautifulSoup(page, 'lxml')
+    table = soup.find('table', class_='ui celled table')
+    trow = table.findAll('tr')
+    test_info = []
+    for row in trow[1:]:
+        col = row.findAll('td')
+        inf = col[0].getText()
+        outf = col[1].getText()
+        test_info.append([inf, outf])
+    return test_info
+
+# print(p.getImageToBase64("https://ooo.0o0.ooo/2017/06/10/593bcc13da98c.png"))
+print(get_test_info('https://loj.ac/problem/506/testdata'))
+# p.getData('https://loj.ac/problem/506/testdata/download/a1.in')
